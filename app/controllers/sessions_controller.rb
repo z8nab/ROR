ce1ba6791d06a41613d9aef06e0210ee8ec7b23d
z8nab <92zainabali@gmail.com>
2015-08-18 18:18:17 +0300

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,14 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(name: params[:name])
   	  if (user.nil? == false) 
-        if user.authenticate(params[:password]) 
-          puts user.authenticate(params[:password])
+        if User.authenticate(params[:password]) 
+          puts User.authenticate(params[:password])
           puts "it's PUTs meth after auth"
           session[:user_id] = user.id
           flash[:success] = "Thanks for logging in!"
           redirect_to admin_url
         else 
-          puts user.authenticate(params[:password])
+          puts User.authenticate(params[:password])
           puts "it's PUTs meth after else, auth"
           flash[:error] = "There was an error logging in. Please check your email and password"
           render action: "new"
