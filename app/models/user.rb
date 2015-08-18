e410105f20f43317bc9e0a2b5fb7a6744cd462a1
z8nab <92zainabali@gmail.com>
2015-08-18 18:08:27 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
 	# has_secure_password
 	def self.authenticate(login, pass)
     		find(:first, :conditions=>["login = ? AND password = ?", login, pass])
+    	end
 end
